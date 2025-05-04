a07d23d86b4eca5828a59fbd09bedd427ab110b5
Cleo <121153199+cleoreyes@users.noreply.github.com>
2025-05-03 17:04:41 -0700

    initial commit

---
+++ iQuiz/QuizTopic.swift
@@ -0,0 +1,14 @@
+//
+//  QuizTopic.swift
+//  iQuiz
+//
+//  Created by Cleo Reyes on 5/3/25.
+//
+
+import UIKit
+
+struct QuizTopic {
+    let icon: UIImage?
+    let title: String
+    let description: String // <-- Here is the description property
+}
--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// Your main ViewController class definition
+class ViewController: UIViewController { // You might also list UITableViewDataSource here, or just in the extension
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let quizTopics: [QuizTopic] = [
+        QuizTopic(icon: UIImage(systemName: "function"), title: "Mathematics", description: "Test your math skills!"),
+        QuizTopic(icon: UIImage(systemName: "atom"), title: "Science", description: "Explore the world of science!"),
+        QuizTopic(icon: UIImage(systemName: "star.fill"), title: "Marvel Super Heroes", description: "Are you a Marvel expert?")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        tableView.dataSource = self // Set the data source
+
+        // tableView.delegate = self // Will add this later for interactions
     }
 
+    // Your @IBAction for the settings button
+    @IBAction func settingsButtonTapped(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: "Settings", message: "Settings go here", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 
+    // ... other methods for your ViewController ...
 }
 
+// *** Put the extension block here, outside the class's {} ***
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of topics
+        return quizTopics.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuizTopicCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "QuizTopicCell")
+
+        let topic = quizTopics[indexPath.row]
+
+        cell.imageView?.image = topic.icon
+        cell.textLabel?.text = topic.title
+        cell.detailTextLabel?.text = topic.description
+        
+        cell.accessoryType = .disclosureIndicator
+
+
+        return cell
+    }
+}
